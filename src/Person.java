7a122ba5cdb200f0b19b4486c88f24b76bfbf32d
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-21 16:21:16 +0700

    method

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,6 @@
 public class Person {
     String name;
-    String address;
-    final String country = "indonesia";
+    void sayHello(String paramName){
+        System.out.println("Hello "+paramName+", My name is "+name);
+    }
 }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -2,11 +2,18 @@ public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
         person1.name = "eko";
-        person1.address = "Jakarta";
-        // person1.country = "USA"; ERROR
 
         System.out.println(person1.name);
-        System.out.println(person1.address);
-        System.out.println(person1.country);
+
+        person1.sayHello("Budi");
+
+        Person person2 = new Person();
+        person2.name = "lisa";
+        person2.sayHello("dini");
+
+        Person person3;
+        person3 = new Person();
+        person3.name = "joko";
+        person3.sayHello("faza");
     }
 }
