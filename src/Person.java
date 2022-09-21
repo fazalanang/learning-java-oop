5aaeb57740ceecf0dad48d0349f05ac1d3ad2a26
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-21 16:32:41 +0700

    constructor

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,13 @@
 public class Person {
     String name;
+    String address;
+    final String country = "indonesia";
+
+    Person (String paramName, String paramAddress) {
+        name = paramName;
+        address = paramAddress;
+    }
+
     void sayHello(String paramName){
         System.out.println("Hello "+paramName+", My name is "+name);
     }
--- src/PersonApp.java
+++ src/PersonApp.java
@@ -1,19 +1,17 @@
 public class PersonApp {
     public static void main(String[] args) {
-        var person1 = new Person();
-        person1.name = "eko";
+        var person1 = new Person("eko","Jakarta");
 
         System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
 
         person1.sayHello("Budi");
 
-        Person person2 = new Person();
-        person2.name = "lisa";
-        person2.sayHello("dini");
+        Person person2 = new Person("lisa","makassar");
 
         Person person3;
-        person3 = new Person();
-        person3.name = "joko";
-        person3.sayHello("faza");
+        person3 = new Person("joko","palembang");
+        person3.sayHello("dika");
     }
 }
