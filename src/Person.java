a1b5d7753fad8c6436eb4fe0186edc15d03c092f
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-21 16:58:13 +0700

    memanggil constructor lain

--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,23 @@
 public class Person {
     String name;
     String address;
+    final String country = "indonesia";
 
     Person (String paramName, String paramAddress) {
         name = paramName;
         address = paramAddress;
     }
+
     Person (String paramName) {
-        name = paramName;
+        this(paramName,null);
     }
+
     Person () {
+        this(null);
+    }
+
+
+    void sayHello(String paramName){
+        System.out.println("Hello "+paramName+", My name is "+name);
     }
 }
