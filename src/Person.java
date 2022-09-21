57fd3e52bd35328b50762a58d694a2d3de72840c
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-21 16:44:48 +0700

    constructor overloading

--- src/Person.java
+++ src/Person.java
@@ -1,14 +1,14 @@
 public class Person {
     String name;
     String address;
-    final String country = "indonesia";
 
     Person (String paramName, String paramAddress) {
         name = paramName;
         address = paramAddress;
     }
-
-    void sayHello(String paramName){
-        System.out.println("Hello "+paramName+", My name is "+name);
+    Person (String paramName) {
+        name = paramName;
+    }
+    Person () {
     }
 }
