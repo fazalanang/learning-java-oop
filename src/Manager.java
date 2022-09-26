b1b7a5aabd4541e37f01ce271ecaa6b1377035fa
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-26 15:47:57 +0700

    Overriding

--- src/Manager.java
+++ src/Manager.java
@@ -1,9 +1,12 @@
 class Manager {
     String name;
     void sayhello (String name) {
-        System.out.println("Hi " + name + ", My name is " + this.name);
+        System.out.println("Hi " + name + ", My name is manager " + this.name);
     }
 }
 class vicePresident extends Manager {
-
+    String name;
+    void sayhello (String name) {
+        System.out.println("Hi " + name + ", My name is vp " + this.name);
+    }
 }
