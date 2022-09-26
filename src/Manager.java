c41012942fb6362f6792e06324e0ba4c8895c99e
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-26 16:49:27 +0700

    super contructor

--- src/Manager.java
+++ src/Manager.java
@@ -1,11 +1,21 @@
 class Manager {
     String name;
+    Manager(String name){
+        this.name = name;
+    }
+    String company;
+    Manager(String name, String company){
+        this.name = name;
+        this.company = company;
+    }
     void sayhello (String name) {
         System.out.println("Hi " + name + ", My name is manager " + this.name);
     }
 }
 class vicePresident extends Manager {
-    String name;
+    vicePresident(String name){
+        super(name);
+    }
     void sayhello (String name) {
         System.out.println("Hi " + name + ", My name is vp " + this.name);
     }
--- src/ManagerApp.java
+++ src/ManagerApp.java
@@ -1,12 +1,10 @@
 public class ManagerApp {
     public static void main(String[] args) {
 
-        var manager = new Manager();
-        manager.name="eko";
+        var manager = new Manager("eko");
         manager.sayhello("budi");
 
-        var vicepresident = new vicePresident();
-        vicepresident.name="joko";
+        var vicepresident = new vicePresident("joko");
         vicepresident.sayhello("budi");
 
     }
