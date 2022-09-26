97a21e5e573fb6039fec91cfeccb6fa2d00e1476
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-26 15:43:09 +0700

    inheritance

---
+++ src/Manager.java
@@ -0,0 +1,9 @@
+class Manager {
+    String name;
+    void sayhello (String name) {
+        System.out.println("Hi " + name + ", My name is " + this.name);
+    }
+}
+class vicePresident extends Manager {
+
+}
---
+++ src/ManagerApp.java
@@ -0,0 +1,13 @@
+public class ManagerApp {
+    public static void main(String[] args) {
+
+        var manager = new Manager();
+        manager.name="eko";
+        manager.sayhello("budi");
+
+        var vicepresident = new vicePresident();
+        vicepresident.name="joko";
+        vicepresident.sayhello("budi");
+
+    }
+}
