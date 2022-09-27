e6449a364b97ac7abaa00dbc1cf985cf428ca3fd
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-27 11:07:54 +0700

    update

--- src/Shape.java
+++ src/Shape.java
@@ -3,12 +3,10 @@ class Shape {
         return 0;
     }
 }
-
 class Rectangel extends Shape {
     int getConner() {
         return 4;
     }
-
     int getParentConner(){
         return super.getConner();
     }
