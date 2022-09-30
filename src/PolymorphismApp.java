9cc38c31d6193700297ab07dce20cd0f3dba4cec
Faza Lanang <faza.lanangdjati@gmail.com>
2022-09-30 16:29:06 +0700

    type check and cast

--- src/PolymorphismApp.java
+++ src/PolymorphismApp.java
@@ -16,6 +16,14 @@ class PolymorphismApp {
     }
 
     static void sayHello(Employee employee){
-        System.out.println("Hello " + employee.name);
+        if (employee instanceof vicePresident) {
+            vicePresident VicePresident = (vicePresident) employee;
+            System.out.println("Hello VP "+ VicePresident.name);
+        } else if (employee instanceof Manager) {
+            Manager manager = (Manager) employee;
+            System.out.println("Hello Manager " + manager.name);
+        }else {
+            System.out.println("Hello " + employee.name);
+        }
     }
 }
