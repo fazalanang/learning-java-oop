249bad6ba7adeb6499c35710366544ea488c3c92
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-04 14:30:59 +0700

    acces modifier

---
+++ src/Matajari/Mitra/Solusi/App/Application.java
@@ -0,0 +1,11 @@
+package Matajari.Mitra.Solusi.App;
+
+import Matajari.Mitra.Solusi.Data.Product;
+
+public class Application {
+    public static void main(String[] args) {
+        Product product = new Product ("Mac book pro",30_000_000);
+        System.out.println(product.name);
+        System.out.println(product.price);
+    }
+}
