dd75d2411ebe1ac8fdf85cd081bd2f0c1f1c922e
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-04 13:46:06 +0700

    package

---
+++ src/Matajari/Mitra/Solusi/Data/Package.java
@@ -0,0 +1,12 @@
+package Matajari.Mitra.Solusi.Data;
+
+public class Package {
+
+    String name;
+    int price;
+
+    Package(String name,int price){
+        this.name = name;
+        this.price = price;
+    }
+}
