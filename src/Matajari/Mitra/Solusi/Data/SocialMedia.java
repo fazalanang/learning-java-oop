f84b31446072b5f705092eff41a7349a23ed07f0
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-07 13:54:21 +0700

    final class

---
+++ src/Matajari/Mitra/Solusi/Data/SocialMedia.java
@@ -0,0 +1,14 @@
+package Matajari.Mitra.Solusi.Data;
+
+class SocialMedia {
+    String name;
+}
+//final class
+final class Facebook extends SocialMedia {
+
+}
+
+/*class FakeFacebook extends Facebook {
+    [ERROR karena Facebook sdh final]
+}*/
+
