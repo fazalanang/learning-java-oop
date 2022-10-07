df2b6c80b09d56fc14ea936351d1a56e1e124d15
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-07 15:45:59 +0700

    inner class

---
+++ src/Matajari/Mitra/Solusi/App/CompanyApp.java
@@ -0,0 +1,25 @@
+package Matajari.Mitra.Solusi.App;
+
+import Matajari.Mitra.Solusi.Data.Company;
+
+public class CompanyApp {
+    public static void main(String[] args) {
+        Company company = new Company();
+        company.setName("Faza Lanang Djati");
+
+        Company.Employee employee = company.new Employee();
+        employee.setName("Faza");
+
+        System.out.println(employee.getName());
+        System.out.println(employee.getCompany());
+
+        Company company2 = new Company();
+        company2.setName("Matajari Mitra Solusi");
+
+        Company.Employee employee2 = company2.new Employee();
+        employee2.setName("Matajari");
+
+        System.out.println(employee2.getName());
+        System.out.println(employee2.getName());
+    }
+}
---
+++ src/Matajari/Mitra/Solusi/Data/Company.java
@@ -0,0 +1,29 @@
+package Matajari.Mitra.Solusi.Data;
+/*Outer Class*/
+public class Company {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    /* Inner class */
+    public class Employee {
+        private String name;
+
+        public String getCompany() {
+            return Company.this.getName();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+}
