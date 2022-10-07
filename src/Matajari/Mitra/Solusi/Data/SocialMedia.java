a4d0ba20628a8babde25bcce224c5bf7a78c9849
Faza Lanang <faza.lanangdjati@gmail.com>
2022-10-07 14:02:24 +0700

    finalMethod

--- src/Matajari/Mitra/Solusi/Data/SocialMedia.java
+++ src/Matajari/Mitra/Solusi/Data/SocialMedia.java
@@ -7,8 +7,19 @@ class SocialMedia {
 final class Facebook extends SocialMedia {
 
 }
-
 /*class FakeFacebook extends Facebook {
     [ERROR karena Facebook sdh final]
 }*/
+
+//final method
+class Instagram extends SocialMedia {
+    final void Login (String userName, String password) {
+        // isi method
+    }
+}
+class fakeInstagram extends Instagram {
+    /*void Login (String userName, String password) {
+        [ERROR karena instagram sdh final void]
+    }*/
+}
 
